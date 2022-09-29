0668d96c95c3e15f7bed246021a6429afd649be0
Joey Calderone <82471424+Acronix98@users.noreply.github.com>
2022-09-29 10:52:32 -0400

    v2.0: Remastered

---
+++ EternityMultiplayer.cs
@@ -0,0 +1,17 @@
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace Eternity
+{
+	public static class EternityMultiplayer
+	{
+		private struct Wait
