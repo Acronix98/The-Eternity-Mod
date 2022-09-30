b905bf3e207048b74b1f06ddd6a65ea020e38c52
Joey Calderone <82471424+Acronix98@users.noreply.github.com>
2022-09-30 10:22:14 -0400

    v2.0: Remastered

---
+++ Utilities/EternityConfig.cs
@@ -0,0 +1,12 @@
+using Eternity.World;
+using System.ComponentModel;
+using System.Runtime.Serialization;
+using Terraria;
+using Terraria.ModLoader.Config;
+
+namespace Eternity.Utilities
+{
+	[Label("Client Config")]
+	class SpiritClientConfig : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ClientSide;
