2ba6da6e2e2d18193bdfcf0ca56a43780a0ca84a
Joey Calderone <82471424+Acronix98@users.noreply.github.com>
2022-10-03 10:31:24 -0400

    v2.0: Remastered

--- Cross/Basic.cs
+++ Cross/Basic.cs
@@ -1,1 +1,1 @@
-The Basic Mod
+The Basic Mod is a Terraria mod for tModLoader created by Acronix98. An API mod that is the successor to base mod, it is not required to have Basic Mod to play Eternity.
