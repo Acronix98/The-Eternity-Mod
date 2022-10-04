7a700be0005388d70685a8a453b0b6d9d87fb68b
Joey Calderone <82471424+Acronix98@users.noreply.github.com>
2022-10-04 13:42:48 -0400

    v2.0: Remastered

---
+++ CrossMod/ModCrossoverContent.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using System;
+using System.Reflection;
+using Eternity;
+using Terraria.ID;
+ 
+using Eternity.Globals;
+using Eternity.Dusts;
+
+namespace Eternity
+{
+    public class ModSupport
