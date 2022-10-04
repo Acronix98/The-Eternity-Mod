4cfda9f224acce39078f6acc1e7f5c9d46944631
Joey Calderone <82471424+Acronix98@users.noreply.github.com>
2022-10-04 13:43:40 -0400

    v2.0: Remastered

---
+++ CrossMod/WeakRefrences.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+using Terraria;
+
+using Eternity;
+using Terraria.ID;
+ 
+using Eternity.Globals;
+using Eternity.Dusts;
+
+namespace Eternity
+{
+    internal class WeakReferences
+    {
